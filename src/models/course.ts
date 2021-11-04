89cf63626c4dde762acd2cff3854281c6421a558
Ivan Soriano <ivan.soriano@live.com.ar>
2021-11-04 18:31:58 -0300

    Add creator email and location

--- src/models/course.ts
+++ src/models/course.ts
@@ -1,50 +1,49 @@
-enum CourseType {
-    type1,
-    type2,
-    type3,
-}
-
-enum SubscriptionType {
-    Platinum,
-    Gold,
-    Silver,
-}
-
 export class Course {
+    creator_email: string;
     title: string;
     description: string;
     hashtags: string[];
-    type: CourseType;
-    sub_type: SubscriptionType;
+    location: string;
+    type: string;
+    sub_type: string;
 
-    constructor(title: string, description: string, hashtags: string[], type: CourseType, sub_type: SubscriptionType) {
+    constructor(email: string, title: string, description: string,
+                 hashtags: string[], location: string, type: string, sub_type: string) {
+        this.creator_email = email;
         this.title = title;
         this.description = description;
         this.hashtags = hashtags;
+        this.location = location;
         this.type = type;
         this.sub_type = sub_type;
-        //TODO: Add location, exams and media
+        //TODO: Add exams and media
         this.check_course_types()
     }
 
     //To verify that the values received in the request are the correct type expected since ts does not enforce it
     check_course_types() {
+        if (typeof this.creator_email != "string") {
+            throw Error("Username should be a string");
+        }
         if (typeof this.title != "string") {
             throw Error("Title should be a string");
         }
         if (typeof this.description != "string") {
             throw Error("Description should be a string");
         }
+        if (typeof this.location != "string") {
+            throw Error("Location should be a string");
+        }
         for (let h of this.hashtags) {
             if (typeof h != "string") {
                 throw Error("hashtags should be strings");
             }
         }
-        if (typeof this.type != "number") {
-            throw Error("type should be a number");//TODO: See if i can verify if it is CourseType instead of just number
+        if (typeof this.type != "string") {
+            throw Error("type should be a string");
         }
-        if (typeof this.sub_type != "number") {
-            throw Error("sub type should be a number");//TODO: Idem above
+        if (typeof this.sub_type != "string") {
+            throw Error("sub type should be a string");
         }
     }
 } 
--- src/server.ts
+++ src/server.ts
@@ -6,35 +6,40 @@ const bodyParser = require('body-parser');
 export default function createServer() {
   const app: Application = express();
 
-  app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  app.get("/", (req: Request, res: Response) => {
     res.send("Hello world!");
   });
 
-  app.get("/ping", (req: Request, res: Response, next: NextFunction) => {
+  app.get("/ping", (req: Request, res: Response) => {
     res.send("pong");
   });
 
-  app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+  app.get("/status", (req: Request, res: Response) => {
     res.send("ok");
-  });
-
-  app.get("/user", (req: Request, res: Response, next: NextFunction) => {
-    res.send("john");
   });
 
   app.use(bodyParser.json());
 
   app.post("/create", (req: Request, res: Response) => {
     try {
-      let course: Course = new Course(req.body.title, req.body.description, req.body.hashtags,
-                                      req.body.type, req.body.sub_type);
+      let course: Course = new Course(req.body.email, req.body.title, req.body.description, req.body.hashtags,
+                                      req.body.location, req.body.type, req.body.sub_type);
       //TODO: Add course to db
+      console.log(course)
     } catch (e) {
       console.log("Error creating course: ", e);
       res.status(405).json({'status': 'error', 'message': 'Missing or invalid fields'});
     }
     res.status(200).json({'status': 'ok', 'message':'course succesfully created'}) //TODO: Should i return the course?
   })
+
+  /*  Curl command to test course creation. Change token once it timeouts
+  curl --request POST \
+  --url http://localhost:8516/courses/create \
+  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1bl9tYWlsX3JhbmRvbUBnbWFpbC5jb20iLCJleHAiOjE2MzU5OTMyNjB9.Ri80aTDX9ZoCpBTW67z8sWTFZMbxvMlCnvXkTH0Dc-A' \
+  --header 'Content-Type: application/json' \
+  --data '{"title":"chau","description":"alto curso", "hashtags":["h1","h2"],"sub_type":"gratis","type":"programacion"}' 
+  */
 
   return app;
 }
