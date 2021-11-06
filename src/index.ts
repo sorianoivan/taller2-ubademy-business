da954011c2c0baf7150655296fb1aabf7026d2b2
Ivan <isoriano@fi.uba.ar>
2021-11-06 20:27:41 -0300

    Add correct response to gateway

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 require('newrelic')
-import express, { Application, Request, Response, NextFunction } from "express";
-import { MongoAPIError } from "mongodb";
-import createServer from "./server";
+import create_server from "./server";
 import * as mongo from "mongodb";
 
+//TODO: get url from process.env
+const MONGOD_URL = "mongodb+srv://ubademy-business:juNU5lALrtGcd9TH@ubademy.t7kej.mongodb.net/Ubademy?retryWrites=true&w=majority";
 
-const startServer = (business_db: mongo.Db) => {
-  const app = createServer(business_db);
+
+const start_server = (business_db: mongo.Db) => {
+  const app = create_server(business_db);
   const port: number = parseInt(<string>process.env.PORT, 10) || 4000;
   return app.listen(port, () => {
     console.log(`server running on port ${port}`);
   });
 };
 
-const MONGOD_URL = "mongodb+srv://ubademy-business:juNU5lALrtGcd9TH@ubademy.t7kej.mongodb.net/Ubademy?retryWrites=true&w=majority";
-const connectToDatabase = () => {
+const connect_to_database = () => {
   const mongo_client = new mongo.MongoClient(MONGOD_URL);
   try {
     mongo_client.connect();
@@ -26,10 +26,10 @@ const connectToDatabase = () => {
   return mongo_client
 }
 
-let mongo_client = connectToDatabase();
+let mongo_client = connect_to_database();
 
 if (mongo_client) {
-  let server = startServer(mongo_client.db(<string>"Business"));
+  let server = start_server(mongo_client.db(<string>"Business"));
   
   //This is to close everything correctly with ctrl + c
   process.on('SIGINT', () => {
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,9 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application, Request, Response } from "express";
 import { Course } from "./models/course";
-const body_parser = require('body-parser');
+import * as body_parser from "body-parser";
 import * as mongoDB from "mongodb";
 
-// const url = "mongodb+srv://ubademy-business:juNU5lALrtGcd9TH@ubademy.t7kej.mongodb.net/Ubademy?retryWrites=true&w=majority";
-// const client = new mongo.MongoClient(url);
-// async function run() {
-//     try {
-//         await client.connect();
-//         console.log("Connected correctly to server");
-//     } catch (err) {
-//         console.log(err);
-//     }
-//     finally {
-//         //await client.close();
-//     }
-// }
-// run().catch(console.dir);
-
-// const business_db = client.db(<string>"Business");
-// //const profiles_table = business_db.collection(<string>process.env.PROFILES_TABLE);
-// const courses_table = business_db.collection(<string>"Courses");
-
-
-export default function createServer(business_db: mongoDB.Db) {
+export default function create_server(business_db: mongoDB.Db) {
   const app: Application = express();
 
   app.get("/", (req: Request, res: Response) => {
@@ -40,36 +20,35 @@ export default function createServer(business_db: mongoDB.Db) {
 
   app.use(body_parser.json());
 
-  app.post("/create", async (req: Request, res: Response) => {
+  app.post("/create_course", async (req: Request, res: Response) => {
     try {
       let course: Course = new Course(req.body.email, req.body.title, req.body.description, req.body.hashtags,
                                       req.body.location, req.body.type, req.body.subscription_type);
-      console.log(course)
-      business_db.collection("Courses").insertOne(course)
-      .then((result: { insertedId: any; }) => console.log(`Successfully inserted item with _id: ${result.insertedId}`))
-      .catch((err: any) => console.error(`Failed to insert item: ${err}`))
-      //await client.close();
-      console.log("chau")
-      //const p = await courses_table.insertOne(course);
-      //console.log(p)
+      console.log(course);//To debug
+      await business_db.collection("Courses").insertOne(course);
+      console.log("Course succesfully inserted");      
     } catch (e) {
       console.log("Error creating course: ", e);
-      res.status(405).json({'status': 'error', 'message': 'Missing or invalid fields'});
-      return;
+      if (e instanceof mongoDB.MongoServerError) {//If the course fails the checks in its constructor it throws Error. TODO: Change to a custom error
+        res.status(202).json({'status': 'error', 'message': 'Could not insert course to db'});
+        return;
+      } else if (e instanceof Error){
+        res.status(201).json({'status': 'error', 'message': 'Missing or invalid fields'});
+        return; 
+      }
     }
-    res.status(200).json({'status': 'ok', 'message':'course succesfully created'}) //TODO: Should i return the course?
+    res.status(200).json({'status': 'ok', 'message':'course succesfully created'}) 
   })
 
-  app.get("/course", async (_req: Request, res: Response) => {
-    try{
-      const my_course = await business_db.collection("Courses").findOne({title: "epico"});
-      // Print to the console
-      console.log(my_course);
-      res.status(200).end();
-    } catch (err) {
-      console.log(err);
-      res.status(406).end();
-    }
+  app.get("/course/", async (req: Request, res: Response) => {
+    // try{
+    //   const my_course = await business_db.collection("Courses").findOne({title: "epico"});
+    //   console.log(my_course);
+    //   res.status(200).end();
+    // } catch (err) {
+    //   console.log(err);
+    //   res.status(406).end();
+    // }
   });
   return app;
 }
