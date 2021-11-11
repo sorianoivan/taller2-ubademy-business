ddfbbfa9367ba5ed2f1602ce444bc8f3716a445b
Ivan Soriano <ivan.soriano@live.com.ar>
2021-11-11 20:54:31 -0300

    Fix test

--- test/test.ts
+++ test/test.ts
@@ -34,7 +34,7 @@ describe("server checks", function () {
 
   it("valid course input", function() {
     const request = {"email":"billgates@gmail.com","title":"microsoft",
-                    "description":"learn windows", "hashtags":["windows","azure"],
+                    "description":"learn windows", "total_exams":3,"hashtags":["windows","azure"],
                     "media":["https://firebasestorage.googleapis.com/v0/b/ubademy-business.appspot.com/o/meme.jpg?alt=media&token=c77bee56-e10c-41b0-ba15-bbdc26a2d974"],
                     "country":"Argentina","subscription_type":"Platinum","course_type":"Programming"};
     let course = new Course(request);
