f73ea1863f7ffa2a91c0e5f8eaaec3ffda2be060
Agustin <aguscambiano@gmail.com>
2021-11-22 16:44:23 -0300

    Added drop_db endpoint

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,10 @@ import { config } from "./configuration/config"
 import { InvalidConstructionParameters } from "./models/invalid_construction_parameters";
 import e from "express";
 const body_parser = require('body-parser');
-const mongo = require("mongodb")
-import { get_profile_schema } from "./lone_schemas/get_profile"
+const mongo = require("mongodb");
+import { get_profile_schema } from "./lone_schemas/get_profile";
+import request from "supertest";
+
 
 //TODO: The link is here because when the tests are run there is no env to take MONGODB_URL from.
 // The string is from the test db
@@ -21,7 +23,7 @@ const MONGO_LONG_ID_LEN = 24
 export function connect_to_database() {
   const mongo_client = new mongo.MongoClient(url);
   try {
-    mongo_client.connect();//Agrego .then()?
+    mongo_client.connect().then({});//Agrego .then()?
     console.log("Connected correctly to server");
   } catch (err) {
     console.log(err);
@@ -36,12 +38,15 @@ export function create_server(business_db: Db) {//Db is the type for a mongo dat
   const profiles_table = business_db.collection(process.env.PROFILES_TABLE || "Profiles");
   const courses_table = business_db.collection("Courses");
 
-  // If the link is form the test db
-  if (url.includes("cluster0")) {
-    profiles_table.deleteMany({}, {});
-    courses_table.deleteMany({}, {});
-  }
 
+  app.get("/drop_db", (req: Request, res: Response) => {
+    // If the link is form the test db
+    if (url.includes("cluster0")) {
+      profiles_table.deleteMany({});
+      courses_table.deleteMany({});
+    }
+    res.send("Ok");
+  });
 
   app.get("/", (req: Request, res: Response) => {
     res.send("Hello world!");
