cb7a3172fbe3d72af4d6061d9bf0c0f1e2774cdf
Agustin <aguscambiano@gmail.com>
2021-12-04 18:23:14 -0300

    Started implementation of filters

--- src/endpoints/courses.ts
+++ src/endpoints/courses.ts
@@ -252,7 +252,7 @@ router.get("/:id/students", async (req: Request, res:Response) => {
 
 router.get("/:id/exams", async (req: Request, res:Response) => {
     let id = req.params.id;
-    const Id = schema(String);
+    const Id = schema(String); //TODO: SE PODRIA CAMBIAR ESTO A UN SCHEMA QUE CHEQUEE EL LARGO DEL STRING
     if (!Id(id) || (id.length != MONGO_SHORT_ID_LEN && id.length != MONGO_LONG_ID_LEN)) {
         res.send(config.get_status_message("invalid_course_id"));
         return;
@@ -267,6 +267,55 @@ router.get("/:id/exams", async (req: Request, res:Response) => {
             exam_names.push(element.exam_names);
         });
         res.send({...config.get_status_message("got_exams_names"), "exams": exam_names});
+    } catch (err) {
+        console.log(err);
+        let message = config.get_status_message("unexpected_error");
+        res.status(message["code"]).send(message);
+    }
+});
+
+router.get("/:id/students_exams/:email/:filter", async (req: Request, res:Response) => {
+    let id = req.params.id;
+    const Id = schema(String); //TODO: SE PODRIA CAMBIAR ESTO A UN SCHEMA QUE CHEQUEE EL LARGO DEL STRING
+    if (!Id(id) || (id.length != MONGO_SHORT_ID_LEN && id.length != MONGO_LONG_ID_LEN)) {
+        res.send(config.get_status_message("invalid_course_id"));
+        return;
+    }
+
+    //TODO: AGREGAR CHEQUEO DE QUE EL MAIL ES DEL CREADOR O DE UN COLABORADOR
+    //TODO: AGREGAR LOS FILTROS
+    //TODO: PROBAR BIEN QUE ESTO ANDE CUANDO SE MERGEE 
+
+    //TODO: AGREGAR SCHEMA Q CHEQUEE LO Q RECIBIMOS EN FILTER
+
+    try {
+        let exams;
+
+        if (req.params.filter === "none") {
+            exams = await exams_table.aggregate(
+                [{"$match": {"$expr": {"$eq":["$_id", new ObjectId(id)]}}},
+                {"$unwind": {"path": "$exams"}},
+                {"$unwind": {"path": "$exams.students_exams"}},
+                {"$project": {
+                    "_id": 0, 
+                    "exam_name": "$exams.exam_name",
+                    "student_email": "$exams.students_exams.student_email",
+                    "status": "$exams.students_exams.mark"
+                }}]).toArray();
+            exams.forEach((element:any) => {
+                if (element.status === -1) {
+                    element.status = "Not graded";
+                } else {
+                    element.status = "Graded";
+                }
+            });
+        } else if (req.params.filter === "graded") {
+
+        } else (req.params.filter === "not_graded") {
+
+        }
+
+        res.send({...config.get_status_message("got_exams_names"), "exams": exams});
     } catch (err) {
         console.log(err);
         let message = config.get_status_message("unexpected_error");
